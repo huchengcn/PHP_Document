0c588beb2a41b4e832b9034764631099cee0cf4b
hucheng <379161098@qq.com>
2015-07-26 10:03:00 +0800

    SMS&Email
    SMS&Email

--- document.php
+++ document.php
@@ -1,9 +1,14 @@
 <?php
 /**
 桂富宝PHP技术文档
-编写者：桂富宝技术团队
+编写者：桂富宝PHP技术团队
 */
 
+//短信发送 $phone为发送的号码  $content为发送的内容
+$this->sendSMS($phone, $content);
+
+//邮件发送 $to为发送邮件地址，$title为邮件标题，$content为发送邮件内容
+$this->email($to, $title, $content);
 
 
 
